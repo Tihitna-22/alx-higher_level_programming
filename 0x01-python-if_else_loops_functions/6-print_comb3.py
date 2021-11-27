63c78e0024ba72e92f52123f5ddbe0e96a0d41e6
Tihitna-22 <tihitna222sisay@gmail.com>
2021-11-27 03:00:44 -0800

    modify 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 for n1 in range(0, 9):
     for n2 in range(n1 + 1, 10):
         print("{:d}{:d}".format(n1, n2), end='')
@@ -7,4 +6,3 @@ for n1 in range(0, 9):
             print(", ", end='')
         else:
             print("")
-            
