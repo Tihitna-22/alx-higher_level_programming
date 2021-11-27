d271c465066c64456914f08f9bbb9e5fe1a4f986
Tihitna-22 <tihitna222sisay@gmail.com>
2021-11-27 03:43:54 -0800

    modify  3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     res = 0
     for i in range(1, alen):
      if alen > 1:
-        res +=  int(sys.argv[i])
+        res += int(sys.argv[i])
         i += 1
     print(res)
