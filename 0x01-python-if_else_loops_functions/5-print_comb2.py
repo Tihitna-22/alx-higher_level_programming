8d5a5bdbae830f5911d615ad0ed22b1e524e91b7
Tihitna-22 <tihitna222sisay@gmail.com>
2021-11-27 02:53:53 -0800

    modify 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 comma = ", "
-for i in range(0, 101):
-    if i == 100:
+for i in range(1, 100):
+    if i == 99:
         comma= ""
     print("{:02d}".format(i),end=comma)
 print(" ")
