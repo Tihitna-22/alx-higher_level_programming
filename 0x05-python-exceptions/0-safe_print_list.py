35a71025266686478bb841b3fbd9b73359109b74
Tihitna-22 <tihitna222sisay@gmail.com>
2021-12-06 17:49:53 -0800

    modify for pycodestyle

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     i = 0
     for a in range(x):
         try:
-            print(my_list[a],end='')
+            print(my_list[a], end='')
             i += 1
         except IndexError:
             break
