3c6232556d1e073e827b2c643a4e009b815c672a
Tihitna-22 <tihitna222sisay@gmail.com>
2021-12-10 01:58:04 -0800

    add 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Square class definition"""
+
+
+class Square:
+    """Represents a square
+    Attributes:
+        __size (int): size of a side of the square
+    """
+    def __init__(self, size=0):
+        """Initializes a square
+            size (int): size of a side of the square
+        Returns: None
+        """
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+        
