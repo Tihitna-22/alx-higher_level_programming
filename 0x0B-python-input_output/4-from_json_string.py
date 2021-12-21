f4d05d4d1443b4479b564c9c2c33b90f87c17298
Tihitna-22 <tihitna222sisay@gmail.com>
2021-12-21 04:47:20 -0800

    add 4-from_json_string.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+"From_json_string" function
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+     returns object (Python data structure) represented by a JSON string:
+    """
+    return json.loads(my_str)
