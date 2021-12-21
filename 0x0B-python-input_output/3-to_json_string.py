c68623be479c10ce24a8a8afefb12915b939a1a7
Tihitna-22 <tihitna222sisay@gmail.com>
2021-12-21 04:27:49 -0800

    modify

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+"to_json_string" function
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+     returns the JSON representation of an object (string)
+    """
+    return json.dump(my_obj)
