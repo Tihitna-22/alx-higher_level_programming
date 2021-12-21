37fca2061b7984800f0e8dd5b53e3887ceefa8bb
Tihitna-22 <tihitna222sisay@gmail.com>
2021-12-21 10:34:28 -0800

    add

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,26 @@
+!/usr/bin/python3
+"""
+Define the class "Student"
+"""
+
+
+class Student:
+    """Representation of a student"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes the student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns a dictionary of a Student instance
+        with specified attributes"""
+        if atr is None:
+            return self.__dict__
+        new_dict = {}
+        for i in atr:
+            try:
+                new_dict[i] = self.__dict__[i]
+            except:
+                pass
+        return new_dict
