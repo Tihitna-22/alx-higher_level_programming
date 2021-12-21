77937ed204802a2a938e4b260cc4c0ac47ffff35
Tihitna-22 <tihitna222sisay@gmail.com>
2021-12-21 01:47:23 -0800

    add 1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/user/bin/python3
+"""
+function writes a string
+"""
+
+
+def write_file(filename="", text=""):
+    """writes a string to a text file (UTF8) and returns the number of characters written"""
+    with open(filename, mode="w", encoding="utf-8") as myFile:
+        return myFile.write(text)
