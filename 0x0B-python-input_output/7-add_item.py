dbc977678a59644bd45080c7aa073687376a3372
Tihitna-22 <tihitna222sisay@gmail.com>
2021-12-21 09:51:02 -0800

    modify for pycodestyle

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 """
-function adds all arguments to a Python list, and then saves them to a file
+add all arguments to a Python list
 """
 from sys import argv
+
 
 if __name__ == "__main__":
     save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
     load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-filename = "add_item.json"
-
 try:
     list = load_from_json_file("add_item.json")
-except:
+except FileNotFoundError:
     list = []
 
 for arg in argv[1:]:
