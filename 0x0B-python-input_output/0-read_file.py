7de451082894dc9137639e698717d09a5026eb61
Tihitna-22 <tihitna222sisay@gmail.com>
2021-12-21 01:42:57 -0800

    add 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/user/bin/python3
+"""
+function that reads a text file
+"""
+
+
+def read_file(filename=""):
+    """ reads a text file (UTF8) and prints it to stdout"""
+    with open(filename, encoding="utf-8") as myFile:
+        print(myFile.read())
