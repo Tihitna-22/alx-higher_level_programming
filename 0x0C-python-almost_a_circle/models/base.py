05f13c0837f7e53bf4efc09ba4282a6fa6922282
Tihitna-22 <tihitna222sisay@gmail.com>
2021-12-28 05:18:29 -0800

    add from_json_string method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,9 +17,18 @@ class Base:
             Base._nb_objects += 1
             self.id = self._nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """returns the JSON string representation of list_dictionaries"""
         if list_dictionaries is None:
             return []
         else:
             return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string representation json_string:"""
+        if json_string is None:
+            return []
+        else:
+            return json.loads(json_string)
