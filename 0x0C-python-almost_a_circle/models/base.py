414a4f945b03658f8fecb89286b1cc8c32b55c4d
Tihitna-22 <tihitna222sisay@gmail.com>
2021-12-30 02:11:20 -0800

    modify for to save and load from cvs fole

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,7 @@
 This module contains the "Base" class
 """
 import json
+import csv
 
 
 class Base:
@@ -58,11 +59,45 @@ class Base:
     def load_from_file(cls):
         filename = cls.__name__ + ".json"
         newlist = []
-        try:
-            with open(filename, 'r') as myFile:
-                newlist = cls.from_json_string(myFile.read())
+        with open(filename, 'r') as myFile:
+            newlist = cls.from_json_string(myFile.read())
             for i, e in enumerate(newlist):
                 newlist[i] = cls.create(**newlist[i])
-        except:
-            pass
+        return newlist
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """save a list of objects represented as csv in a file"""
+        with open(cls.__name__ + '.csv', 'w') as myFile:
+            text = ''
+            for i in list_objs:
+                if cls.__name__ == 'Square':
+                    stri = '{:d},{:d},{:d},{:d}\n'
+                    text += stri.format(i.id, i.size, i.x, i.y)
+                else:
+                    stri = '{:d},{:d},{:d},{:d},{:d}\n'
+                    text += stri.format(i.id, i.width, i.height, i.x, i.y)
+            myFile.write(text)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """deserializes a list of Rectangles/Squares in csv"""
+        filename = cls.__name__ + ".csv"
+        newlist = []
+        with open(filename, 'r') as myFile:
+            csv_read = csv.reader(myFile)
+            for args in csv_read:
+                if cls.__name__ == "Rectangle":
+                    dictionary = {"id": int(args[0]),
+                                  "width": int(args[1]),
+                                  "height": int(args[2]),
+                                  "x": int(args[3]),
+                                  "y": int(args[4])}
+                elif cls.__name__ == "Square":
+                    dictionary = {"id": int(args[0]),
+                                  "size": int(args[1]),
+                                  "x": int(args[2]),
+                                  "y": int(args[3])}
+                obj = cls.create(**dictionary)
+                newlist.append(obj)
         return newlist
