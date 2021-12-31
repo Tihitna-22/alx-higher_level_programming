8b8b9ae0cdd1b8c4b70804f24ba3092355ce0ec2
Tihitna-22 <tihitna222sisay@gmail.com>
2021-12-31 05:18:40 -0800

    add tstcase for base.py

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+import unittest
+import json
+from base import Base
+
+
+class TestBase(unittest.TestCase):
+    """Tests to check Base class"""
+    def test_no_id(self):
+        """Tests id as None"""
+        bs = Base()
+        self.assertEqual(bs.id, 1)
+
+    def test_no_id_after_set(self):
+        """Tests id as None after not None"""
+        bs2 = Base()
+        self.assertEqual(bs2.id, 2)
+
+    def test_to_json_string(self):
+        """Tests regular to json string"""
+        Base._Base__nb_objects = 0
+        d1 = {"id": 6, "width": 3, "height": 4, "x": 5, "y": 6}
+        d2 = {"id": 5, "width": 4, "height": 6, "x": 7, "y": 0}
+        json_s = Base.to_json_string([d1, d2])
+        self.assertTrue(type(json_s) is str)
+        a = json.loads(json_s)
+        self.assertEqual(a, [d1, d2])
+
+    def test_empty_to_json_string(self):
+        """Test for passing empty list/ None"""
+        json_s = Base.to_json_string([])
+        self.assertTrue(type(json_s) is str)
+        self.assertEqual(json_s, "[]")
+
+    def test_None_to_json_String(self):
+        json_s = Base.to_json_string(None)
+        self.assertEqual(json_s, [])
