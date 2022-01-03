28d81c37f29b918ca66aa33071c4694ed1af796f
Tihitna-22 <tihitna222sisay@gmail.com>
2022-01-03 04:28:03 -0800

    modify to json str metod for None

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -8,21 +8,21 @@ import csv
 
 class Base:
     """A base class"""
-    _nb_objects = 0
+    __nb_objects = 0
 
     def __init__(self, id=None):
         """Initialize the base class"""
         if id is not None:
             self.id = id
         else:
-            Base._nb_objects += 1
-            self.id = self._nb_objects
+            Base.__nb_objects += 1
+            self.id = self.__nb_objects
 
     @staticmethod
     def to_json_string(list_dictionaries):
         """returns the JSON string representation of list_dictionaries"""
         if list_dictionaries is None:
-            list_dictionaries = []
+            return "[]"
         else:
             return json.dumps(list_dictionaries)
 
