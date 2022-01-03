b5f038af1ec1d80adcead0c3d959df7f3d7e0e66
Tihitna-22 <tihitna222sisay@gmail.com>
2022-01-03 02:59:13 -0800

    modify for empty list for to json str method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,7 +21,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """returns the JSON string representation of list_dictionaries"""
-        if list_dictionaries is None:
+        if list_dictionaries is None or list_dictionaries == []:
             return []
         else:
             return json.dumps(list_dictionaries)
