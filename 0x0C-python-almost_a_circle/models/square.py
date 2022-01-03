35cb2d8bcedbdc54a6f68b1d0f9b85d8c38a5629
Tihitna-22 <tihitna222sisay@gmail.com>
2022-01-03 01:09:21 -0800

    update init method

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,8 +6,9 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
-        """initializes the square"""
+        """init the square"""
         super().__init__(size, size, x, y)
+        self.size = size
 
     def __str__(self):
         """string representation of the square"""
