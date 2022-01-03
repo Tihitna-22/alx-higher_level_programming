02778cf8c203079bcf0159494251e96b26152a14
Tihitna-22 <tihitna222sisay@gmail.com>
2022-01-03 01:27:35 -0800

    modify init method for id

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,7 +7,7 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """init the square"""
-        super().__init__(size, size, x, y)
+        super().__init__(size, size, x, y, id)
         self.size = size
 
     def __str__(self):
