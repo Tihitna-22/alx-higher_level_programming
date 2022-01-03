346cc54575f4b012e9afada6ac56eb24fcd86cdc
Tihitna-22 <tihitna222sisay@gmail.com>
2022-01-03 02:47:18 -0800

    update for setter of size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,6 +5,7 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """square class"""
     def __init__(self, size, x=0, y=0, id=None):
         """init the square"""
         super().__init__(size, size, x, y, id)
@@ -18,6 +19,10 @@ class Square(Rectangle):
     @size.setter
     def size(self, value):
         """sets the  size"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.width = value
         self.height = value
 
