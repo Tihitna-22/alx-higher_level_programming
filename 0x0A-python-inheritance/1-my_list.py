c72b51274d5eefd8ecc54ed2de960d6d800c7755
Tihitna-22 <tihitna222sisay@gmail.com>
2022-01-06 05:23:35 -0800

    add init for obj

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,6 +6,10 @@ class MyList(list):
     Defines  MyList class hat inherits from list
     """
 
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
+
     def print_sorted(self):
         """
         prints the list, but sorted (ascending sort)
