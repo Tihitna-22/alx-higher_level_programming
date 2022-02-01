a822b8c9956659b76e482a21bba721f6a799c824
Tihitna-22 <tihitna222sisay@gmail.com>
2022-02-01 04:12:00 -0800

    capitalize command

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,2 +1,2 @@
--- script that deletes the database hbtn_0c_0 in MySQL server.
-DROP database IF EXISTS hbtn_0c_0;
+-- Script that deletes the database hbtn_0c_0 in MySQL server.
+DROP DATABASE IF EXISTS hbtn_0c_0;
