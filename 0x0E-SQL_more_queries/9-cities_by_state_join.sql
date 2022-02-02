352698241ad52e29c34f20905f5c67a6e1c567ee
Tihitna-22 <tihitna222sisay@gmail.com>
2022-02-02 06:27:45 -0800

    add 9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -3,5 +3,5 @@
 -- Results must be sorted in ascending order by cities.id
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
-SELECT  cities.id, cities.name states.name FROM cities
+SELECT cities.id, cities.name states.name FROM states
 join cities ON cities.state_id = states.id;
