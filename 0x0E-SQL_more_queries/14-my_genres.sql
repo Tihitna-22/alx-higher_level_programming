914f1c27da7efb5ba1e37f8448d531549bee215e
Tihitna-22 <tihitna222sisay@gmail.com>
2022-02-03 03:03:13 -0800

    modify for order

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -6,5 +6,7 @@
 -- The database name will be passed as an argument of the mysql command
 SELECT tv_genres.name FROM tv_genres
 LEFT OUTER JOIN tv_show_genres
-ON tv_genres.id = tv_shows.id WHERE tv_shows.title = 'Dexter'
-ORDER BY tv_genres.name;
+ON tv_genres.id = tv_show_genres.genre_id
+LEFT OUTER JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name ASC;
