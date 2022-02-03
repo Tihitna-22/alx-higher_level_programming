f27a342e01281c45603f972e6c46742a296307a0
Tihitna-22 <tihitna222sisay@gmail.com>
2022-02-03 03:42:30 -0800

    modify

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -8,4 +8,5 @@ FROM tv_genres LEFT OUTER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 LEFT OUTER JOIN tv_show_ratings
 ON tv_show_genres.show_id = tv_show_ratings.show_id
-GROUP BY tv_genres.name;
+GROUP BY tv_genres.name
+ORDER BY rating DESC;
