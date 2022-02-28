b0706322400824a3422a7001aa9b2e7d1f6b6422
tihitna22 <tihitna222sisay@gmail.com>
2022-02-28 21:36:35 +0000

    modiy or doc

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -4,7 +4,7 @@ let arg = process.argv[2];
 let arg2 = process.argv[3];
 if (arg === undefined) {
   console.log('undefined is undefined');
-} else if (aLen === 3 ){
+} else if (aLen === 3 ) {
   console.log(arg + ' is undefined');
 }else {
   console.log(arg + ' is ' + arg2);
