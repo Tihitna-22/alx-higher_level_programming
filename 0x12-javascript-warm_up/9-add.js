a00cd5b0c0eb334d6736be6081176dd6dc2662ed
tihitna22 <tihitna222sisay@gmail.com>
2022-02-28 20:21:44 +0000

    modify 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
 function add (a, b) {
-    if (isNaN(a) || isNaN(b)) {
-        return (NaN);
-      } else {
-          console.log(parseInt(a) + parseInt(b));
-      }
-  }
-  
+  console.log(parseInt(a) + parseInt(b));
+}
+
 add(process.argv[2], process.argv[3]);
