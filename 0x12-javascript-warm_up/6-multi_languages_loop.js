0cfb8a0ee979fdb248c8086fbc9c6fa692b060cc
tihitna22 <tihitna222sisay@gmail.com>
2022-02-28 21:32:58 +0000

    modiy or documentation

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
 for (let i = 0; i < arr.length; i++) {
   console.log(arr[i]);
