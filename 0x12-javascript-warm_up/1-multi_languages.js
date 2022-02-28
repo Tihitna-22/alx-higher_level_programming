4ce1cb494a6fb4bb52bf09f8dfcadf9bd122c0fd
tihitna22 <tihitna222sisay@gmail.com>
2022-02-28 18:06:34 +0000

    add 1-multi_languages.js

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+console.log('C is fun');
+console.log('Python is cool');
+console.log('Javascript is amazing');#!/usr/bin/node
+console.log('C is fun');
+console.log('Python is cool');
+console.log('Javascript is amazing');
