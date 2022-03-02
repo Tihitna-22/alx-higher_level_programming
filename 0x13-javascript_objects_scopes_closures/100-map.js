510d284861ad2693c6fa76fd2c0ea4d579924010
tihitna22 <tihitna222sisay@gmail.com>
2022-03-02 01:45:27 +0000

    fix for documentation

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-let list = require('./100-data').list;
-
+const list = require('./100-data').list;
 console.log(list);
 const map1 = list.map((x, index) => x * index);
 console.log(map1);
