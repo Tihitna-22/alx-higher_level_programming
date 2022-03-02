932126624c969d9856fe24facfb51e243c8e0895
tihitna22 <tihitna222sisay@gmail.com>
2022-03-02 01:38:38 +0000

    modiy for docmentation

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -12,18 +12,4 @@ module.exports = class Square extends Sqr {
       }
     }
   }
-};#!/usr/bin/node
-
-const Sqr = require('./5-square.js');
-
-module.exports = class Square extends Sqr {
-  charPrint (c) {
-    if (typeof c === 'undefined') {
-      this.print();
-    } else {
-      for (let i = 0; i < this.width; i++) {
-        console.log(c.repeat(this.height));
-      }
-    }
-  }
-};
+}
