ba158133be5287716a528d125470961d3dab60b7
tihitna-22 <tihitna222sisay@gmail.com>
2022-03-20 02:38:12 -0700

    add 6-post_email.py

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+takes in a URL and an email, sends a POST request to the passed URL
+with the email as a parameter, and displays the body of the
+response
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    """
+    takes in a URL and an email, sends a POST request to the passed
+    URL with the email as a parameter, and displays the body of the
+    response """
+    url = argv[1]
+    r = requests.post(url, data={'email': argv[2]})
+    print(r.text)
