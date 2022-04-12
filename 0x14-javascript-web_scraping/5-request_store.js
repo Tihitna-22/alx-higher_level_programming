7e968711da4a79e6fa28df12dda0dbf5aab61abe
tihitna-22 <tihitna222sisay@gmail.com>
2022-04-12 14:23:15 -0700

    add 5-request_store.js

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+//script that gets the contents of a webpage and stores it in a file.
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const filePath = process.argv[3];
+request(url, function (err, response, body) {
+  if (err) {
+      console.log(err);	
+        } else {
+	    fs.writeFile(filePath, body, 'utf-8');
+	      }
+	      });
