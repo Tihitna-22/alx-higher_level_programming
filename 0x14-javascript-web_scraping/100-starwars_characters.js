b86f8d1e024955e2f52f8d0627b24d501daa0ccb
tihitna-22 <tihitna222sisay@gmail.com>
2022-04-12 14:50:31 -0700

    add 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,14 +2,14 @@
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
-    if (!error) {
-	const characters = JSON.parse(body).characters;
-	characters.forEach((character) => {
-	    request(character, function (error, response, body) {
-		if (!error) {
-		    console.log(JSON.parse(body).name);
-		}
-	    });
-	});
-    }
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    characters.forEach((character) => {
+      request(character, function (error, response, body) {
+	if (!error) {
+	  console.log(JSON.parse(body).name);
+	}
+      });
+    });
+  }
 });
