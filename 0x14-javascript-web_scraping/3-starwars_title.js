c9ae8778b1deeaee879c4622b81ee78d6b54ac46
tihitna-22 <tihitna222sisay@gmail.com>
2022-04-12 15:26:46 -0700

    mmodify

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,13 +1,7 @@
 #!/usr/bin/node
 // script that prints the title of a Star Wars movie where the episode number matches a given integer.
 const request = require('request');
-const id = process.argv[2];
-const url = 'http://swapi.co/api/films/' + id;
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    const jsonDict = JSON.parse(body);
-    console.log(jsonDict.title);
-  }
+  console.log(error || JSON.parse(body).title);
 });
